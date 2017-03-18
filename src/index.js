42fbecff4c8f96bdfb018460bc8dbccece24ee4b
Rachel Li <825255583@qq.com>
2017-03-18 10:08:29 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        app
+      </div>
+    )
+  }
+}
+
+
+export default App
+ReactDOM.render(<App/>,document.getElementById('box'));
